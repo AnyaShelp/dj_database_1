b5b7083a3adeba498c8e0fc05cece1c3ff841b2a
AnyaShelp <103931996+AnyaShelp@users.noreply.github.com>
2023-06-05 10:08:02 +0300

    Update models.py

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Phone(models.Model):
-    id = models.IntegerField(primary_key=True,)
     name = models.CharField(max_length=50)
-    price = models.IntegerField()
-    image = models.URLField()
-    release_date = models.DateField()
+    price = models.IntegerField(null=False)
+    image = models.ImageField()
+    release_date = models.DateField(max_length=10)
     lte_exists = models.BooleanField()
-    slug = models.SlugField(null=True, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
